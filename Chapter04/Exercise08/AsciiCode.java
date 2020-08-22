0696bb9779b232d4440203f4bfdc14b4b35a6934
VPawlowski <62677799+VPawlowski@users.noreply.github.com>
2020-08-22 15:10:29 -0300

    Update AsciiCode.java

--- Chapter04/Exercise08/AsciiCode.java
+++ Chapter04/Exercise08/AsciiCode.java
@@ -1,6 +1,6 @@
 package com.liang.y.daniel.chapter.four;
 
-// Write a program that receives a character and displays its ASCII code (an integer between 0 and 127).
+// Write a program that receives a character and displays its ASCII code.
 
 import java.util.Scanner;
 
@@ -8,7 +8,7 @@ public class AsciiCode {
     public static void main(String[] args) {
         System.out.print("Enter a character: ");
         Scanner in = new Scanner(System.in);
-        char userInput = in.next().charAt(0);
-        System.out.println("The Unicode for the character " + userInput + " is: " +(int) userInput);
+        int userInput = in.nextInt();
+        System.out.println("The ASCII code for " + userInput + " is: " + (char) userInput);
     }
 }
